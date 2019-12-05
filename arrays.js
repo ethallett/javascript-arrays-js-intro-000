7935ce86b16134dddc791517188bafa39672666d
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-05 00:45:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray() {
 
 function destructivelyAddElementToBeginningOfArray(){
   var array = [1]
-  return array.unshift("foo");
+  return(array.unshift("foo"));
 }
 
 function addElementToEndOfArray() {
